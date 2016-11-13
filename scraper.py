5dcf50eb6a38d01b6497302233d033f0ff938464
chris48s <c.mshaw@yahoo.co.uk>
2016-11-13 11:48:52 +0000

    init

---
+++ scraper.py
@@ -0,0 +1,12 @@
+from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
+
+
+stations_url = "https://mapping.newham.gov.uk/ArcGIS/rest/services/Polling_Station_Finder14/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=OBJECTID%2CPLACE_REFE%2CPLACE_PNAM%2CPLACE_ADD1%2CPLACE_ADD2%2CPLACE_ADD3%2CPLACE_PCOD&f=kmz"
+districts_url = "https://mapping.newham.gov.uk/ArcGIS/rest/services/Polling_Station_Finder14/MapServer/1/query?text=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=OBJECTID+LIKE+%27%25%27&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=OBJECTID%2CPLACE_REFE%2CPLACE_PNAM%2CPLACE_ADD1%2CPLACE_ADD2%2CPLACE_ADD3%2CPLACE_PCOD&f=kmz"
+council_id = 'E09000025'
+
+
+stations_scraper = HashOnlyScraper(stations_url, council_id, 'stations')
+stations_scraper.scrape()
+districts_scraper = HashOnlyScraper(districts_url, council_id, 'districts')
+districts_scraper.scrape()
