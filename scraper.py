2d5db804b150de9044d38c42df7d27ba72900cad
chris48s <chris.shaw480@gmail.com>
2018-01-06 20:29:00 +0000

    catch error response with 200 status code and raise Exception

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from dc_base_scrapers.common import get_data_from_url
 from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
@@ -6,7 +7,20 @@ districts_url = "https://mapping.newham.gov.uk/ArcGIS/rest/services/Polling_Stat
 council_id = 'E09000025'
 
 
-stations_scraper = HashOnlyScraper(stations_url, council_id, 'stations', 'kmz')
+class NewhamHashOnlyScraper(HashOnlyScraper):
+
+    def get_data(self):
+        data = get_data_from_url(self.url)
+        """
+        Newham's API occasionally serves up an error response
+        with a 200 OK status code. Handle this error instead of doing this
+        https://github.com/wdiv-scrapers/data/blob/3ae9425ba145239b9a9fad4df4ef0c21dddf5044/E09000025/stations.kmz
+        """
+        if b'does not exist or is inaccessible' in data and b'Invalid URL' in data:
+            raise Exception('Bad response')
+        return data
+
+stations_scraper = NewhamHashOnlyScraper(stations_url, council_id, 'stations', 'kmz')
 stations_scraper.scrape()
-districts_scraper = HashOnlyScraper(districts_url, council_id, 'districts', 'kmz')
+districts_scraper = NewhamHashOnlyScraper(districts_url, council_id, 'districts', 'kmz')
 districts_scraper.scrape()
